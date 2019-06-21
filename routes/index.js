8ae6fff06d03e91a2385d0fae23feee69cd97790
Ubuntu <ubuntu@ip-172-31-26-239.us-east-2.compute.internal>
2019-06-21 01:22:34 +0000

    fixed path variable bug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var path = require(path);
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,11 +12,11 @@ router.get('/about', function(req, res, next) {
 });
 
 router.get('/projects', function(req, res, next) {
-  res.download('./file_storage/resume.pdf', 'resume.pdf');
+  res.render('projects');
 });
 
 router.get('/download/resume', function(req, res, next) {
-  res.render('projects');
+  res.download('./file_storage/resume.pdf', 'resume.pdf');
 });
 
 router.get('/tankwar', function(req, res, next) {
